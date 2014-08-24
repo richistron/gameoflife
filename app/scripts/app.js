6065a515bf296e8d55d4e0b65af70bc559a52da3
Ricardo Rivas G <richistron@gmail.com>
2014-08-24 14:11:55 -0500

    Scaffold added

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gameOfLifeApp
+ * @description
+ * # gameOfLifeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gameOfLifeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gameOfLifeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gameOfLifeApp
+ */
+angular.module('gameOfLifeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
